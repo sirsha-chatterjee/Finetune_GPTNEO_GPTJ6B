b1c7a1eb8c5ac6b143c140eb13d3a5bb721323ca
sirsha-chatterjee <52195280+sirsha-chatterjee@users.noreply.github.com>
2022-11-24 17:40:53 +0530

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #general requirements
-conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia -y
+#conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia -y
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 pip install datasets==1.16.1
 pip install transformers==4.14.1
 #need to drastically lower requirements
